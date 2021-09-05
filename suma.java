4d77d5d8101756874ea47fcae70328b90907ac58
Toni Fernandez <toni.fernandez@cirvianum.cat>
2021-09-05 11:27:48 +0200

    S'ha creat arxiu suma.java
    S'ha escrit el codi però esta incomplert falta el print per veure el resutlat

---
+++ suma.java
@@ -0,0 +1,8 @@
+int numero1=0;
+int numero2=0;
+
+System.out.println("Introdueix el 1r nombre");
+nuemro1 = reader.nextInt();
+
+System.out.println("Introdueix el 2n nombre");
+nuemro2 = reader.nextInt();
